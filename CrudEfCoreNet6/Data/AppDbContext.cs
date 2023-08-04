607b5f3583d2aace5872c8bf4739c38b3f066c8e
lsiepsiak <lsiepsiak@gmail.com>
2023-08-04 14:56:55 +0200

    Add db context

---
+++ CrudEfCoreNet6/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudEfCoreNet6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudEfCoreNet6.Data;
+
+public class AppDbContext: DbContext
+{
+    public DbSet<Team> Teams { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
