ddb06d3dad1a0bdbbfa7c2c5c8b72b5cb8bed594
lsiepsiak <lsiepsiak@gmail.com>
2023-08-05 07:47:32 +0200

    Connect in memory EF database and use it via AppDbContext

--- CrudEfCoreNet6/Controllers/TeamController.cs
+++ CrudEfCoreNet6/Controllers/TeamController.cs
@@ -1,5 +1,7 @@
-﻿using CrudEfCoreNet6.Models;
+﻿using CrudEfCoreNet6.Data;
+using CrudEfCoreNet6.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CrudEfCoreNet6.Controllers;
 
@@ -7,41 +9,25 @@ namespace CrudEfCoreNet6.Controllers;
 [Route("[controller]")]
 public class TeamController: ControllerBase
 {
-    private static List<Team> _teams = new List<Team>
+    private static AppDbContext _context;
+
+    public TeamController(AppDbContext context)
     {
-        new Team()
-        {
-            Id = 1,
-            Country = "Poland",
-            Name = "Fiat",
-            TeamPrinciple = "Fiat is the best!"
-        },
-        new Team()
-        {
-            Id = 2,
-            Country = "England",
-            Name = "Ferrari",
-            TeamPrinciple = "Ferrari rules!"
-        },
-        new Team()
-        {
-            Id = 3,
-            Country = "Italy",
-            Name = "Alfa Romeo",
-            TeamPrinciple = "Romeo best!"
-        }
-    };
+        _context = context;
+    }
   
     [HttpGet]
-    public IActionResult Get()
+    public async Task<IActionResult> Get()
     {
-        return Ok(_teams);
+        var teams = await _context.Teams.ToListAsync();
+        
+        return Ok(teams);
     }
 
     [HttpGet("{id:int}")]
-    public IActionResult Get(int id)
+    public async Task<IActionResult> Get(int id)
     {
-        var team = _teams.FirstOrDefault(x => x.Id == id);
+        var team = await _context.Teams.FirstOrDefaultAsync(x => x.Id == id);
 
         if (team == null)
             return BadRequest($"There is no team in this ID: {id}");
@@ -50,20 +36,21 @@ public class TeamController: ControllerBase
     }
 
     [HttpPost]
-    public IActionResult Post(Team team)
+    public async Task<IActionResult> Post(Team team)
     {
         if (team == null)
             return BadRequest("You cannot specify team");
         
-        _teams.Add(team);
+        await _context.Teams.AddAsync(team);
+        await _context.SaveChangesAsync();
 
         return CreatedAtAction("Get", team.Id, team);
     }
 
     [HttpPut]
-    public IActionResult Put(int id, string country)
+    public async Task<IActionResult> Put(int id, string country)
     {
-        var team = _teams.FirstOrDefault(team => team.Id == id);
+        var team = await _context.Teams.FirstOrDefaultAsync(team => team.Id == id);
         
         if (team == null)
             return BadRequest($"Team with ID:{id} does not exists!");
@@ -72,19 +59,21 @@ public class TeamController: ControllerBase
             return BadRequest("You must correctly specify country property!");
         
         team.Country = country;
+        await _context.SaveChangesAsync();
 
         return NoContent();
     }
 
     [HttpDelete]
-    public IActionResult Delete(int id)
+    public async Task<IActionResult> Delete(int id)
     {
-        var team = _teams.FirstOrDefault(team => team.Id == id);
+        var team = await _context.Teams.FirstOrDefaultAsync(team => team.Id == id);
         
         if (team == null)
             return BadRequest($"Team with ID:{id} does not exists!");
 
-        _teams.Remove(team);
+        _context.Teams.Remove(team);
+        await _context.SaveChangesAsync();
 
         return NoContent();
     }
